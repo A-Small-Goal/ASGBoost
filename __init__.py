8d556ae31685ea0ddcd30915efec394ab57c7a22
Tianyu Wang <twangad@connect.ust.hk>
2016-09-12 17:57:33 -0400

    first commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#
+
+__all__ = ['tree']
+# deprecated to keep older scripts who import this from breaking
+from ASGBoost.tree import tree
---
+++ tree.py
@@ -0,0 +1,10 @@
+###
+
+
+class tree:
+
+	def __init__(self, a=1):
+		self.a = a
+
+	def saysth(self, s='here'):
+		print s
